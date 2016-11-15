8dfd3104f644fe4cfe9a38208c54fd086214235f
hugo <hugo.develop@gmail.com>
2016-11-15 22:17:23 +0100

    Add files via upload

---
+++ AplicacionConsola1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionConsola1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Salida por pantalla
+            Console.WriteLine("Muestra un mensaje, y pulsa una tecla para salir.");
+            //Pulsa una tecla para salir
+            Console.ReadKey(); 
+        }
+    }
+}
